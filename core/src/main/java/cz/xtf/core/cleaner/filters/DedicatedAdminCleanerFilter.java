ca50dbc59f0691452aa5a1591cf5d6e78688c5c7
Kabir Khan <kkhan@redhat.com>
2023-08-25 12:02:43 +0200

    Subject name seems a better filter for dedicated admins

--- core/src/main/java/cz/xtf/core/cleaner/filters/DedicatedAdminCleanerFilter.java
+++ core/src/main/java/cz/xtf/core/cleaner/filters/DedicatedAdminCleanerFilter.java
@@ -1,6 +1,9 @@
 package cz.xtf.core.cleaner.filters;
 
+import java.util.Set;
+
 import io.fabric8.kubernetes.api.model.rbac.RoleBinding;
+import io.fabric8.kubernetes.api.model.rbac.Subject;
 
 /**
  * On OpenShift sandbox we sometimes, but not always, some extra rolebindings appear in the namespace.
@@ -25,11 +28,50 @@ import io.fabric8.kubernetes.api.model.rbac.RoleBinding;
  * </pre>
  * 
  * Which is in line with what is mentioned in
- * https://docs.openshift.com/container-platform/4.8/support/gathering-cluster-data.html
+ * https://docs.openshift.com/dedicated/3/admin_guide/manage_rbac.html
+ *
+ * However, there are some other occurrences which have a different roleRef, here the subject seems to be one of the following
+ * two:
+ * 
+ * <pre>
+ * subjects:
+ * - apiGroup: rbac.authorization.k8s.io
+ *   kind: Group
+ *   name: system:serviceaccounts:dedicated-admin
+ * </pre>
+ * 
+ * or
+ * 
+ * <pre>
+ * subjects:
+ * - apiGroup: rbac.authorization.k8s.io
+ *   kind: Group
+ *   name: dedicated-admins
+ * </pre>
+ *
+ * The above subject entries show up both when the roleRef matches the above or not, so the subject seems to be the
+ * best thing to filter on.
  */
 public class DedicatedAdminCleanerFilter implements OpenShiftCleanerFilter {
+
+    private static final Set<String> SUBJECT_NAMES = Set.of("dedicated-admins", "system:serviceaccounts:dedicated-admin");
+
     @Override
     public boolean filterRoleBinding(RoleBinding rb) {
-        return !rb.getRoleRef().getName().equals("dedicated-admins-project");
+        for (Subject subject : rb.getSubjects()) {
+            String kind = subject.getKind();
+            if (!kind.equals("Group")) {
+                continue;
+            }
+            String apiGroup = subject.getApiGroup();
+            if (!apiGroup.contains("rbac.authorization.k8s.io")) {
+                continue;
+            }
+            String name = subject.getName();
+            if (SUBJECT_NAMES.contains(name)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
